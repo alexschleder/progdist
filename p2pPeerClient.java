ac21caf8b7794d55b782f1f9911bd82afbffe682
jonasCCa <jonas.ccampos@hotmail.com>
2022-04-17 19:11:30 -0300

    novo sistwema de inpuwut

--- p2pPeerClient.java
+++ p2pPeerClient.java
@@ -20,6 +20,10 @@ public class p2pPeerClient extends Thread
 		serverInterface = serverIf;
 	}
 
+	private enum inputType {
+		LIST, LIST_SEARCH, FILE, INVALID;
+	}
+
 	public void run() 
 	{
 		BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
@@ -27,18 +31,51 @@ public class p2pPeerClient extends Thread
 
 		while (true) 
 		{
+			inputType curType = inputType.INVALID;
 
 			System.out.println("\n<list/peer> <message> <ip>");
 			System.out.println("Example: list user <server_ip>");
 			System.out.println("Example: peer \"hello_world!\" <peer_ip> <port>");
 			try 
 			{
-				
-
-
 				str = obj.readLine();
 				String vars[] = str.split("\\s");
-				addr = InetAddress.getByName(vars[2]);
+
+				if(vars[0].equals("list")) {
+					if(vars.length == 1) {
+						curType = inputType.LIST;
+					} else {
+						curType = inputType.LIST_SEARCH;
+					}
+				} else if(vars[0].equals("peer")) {
+					curType = inputType.FILE;
+				}
+
+				switch(curType) {
+					case LIST:
+						ArrayList<Peer> recursos = serverInterface.listResources(null);
+						System.out.println("-=-=-=-\nResources: \n");
+						for(Peer p : recursos) {
+							System.out.println(p.toString());
+						}
+						break;
+					case LIST_SEARCH:
+						ArrayList<Peer> recursosSearch = serverInterface.listResources(vars[1]);
+						System.out.println("-=-=-=-\nResources with term \"" + vars[1] + "\": \n");
+						for(Peer p : recursosSearch) {
+							System.out.println(p.toString());
+						}
+						break;
+					case FILE:
+						//comunicacao entre peers, baixar arquivo
+						break;
+					case INVALID:
+						System.out.println("\tError: Invalid \"" + vars[0] + "\" command\n");
+						break;
+					default:
+				}
+
+				/*addr = InetAddress.getByName(vars[2]);
 				String str2 = vars[0] + " " + vars[1];
 				resource = str2.getBytes();
 				if (vars.length == 4) 
@@ -49,12 +86,13 @@ public class p2pPeerClient extends Thread
 				else 
 				{
 					peer_port = 9000;
-				}
+				}*/
 			} 
 			catch (IOException e) 
 			{
 			}
 			
+			//comentar? colocar if?
 			try 
 			{
 				packet = new DatagramPacket(resource, resource.length, addr, peer_port);
